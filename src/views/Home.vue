995c367cc4d7bec88d3721041bf683451b739f7a
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-02-06 16:05:27 +0900

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from "@/components/Pokemon.vue";
+
+export default {
+  name: "home",
+  components: {
+    Pokemon
+  }
+};
+</script>
