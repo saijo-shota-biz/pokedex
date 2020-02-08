6b1023cce058aa772889773daf7e87342d9e4c3d
saijo-shota-biz <60728259+saijo-shota-biz@users.noreply.github.com>
2020-02-09 01:51:05 +0900

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath:
-    process.env.NODE_ENV === "production" ? "saijo-shota-biz/pokedex/" : "/",
+    process.env.NODE_ENV === "production" ? "/saijo-shota-biz/pokedex/" : "/",
   outputDir: "docs"
 };
